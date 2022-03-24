54e45bf2fcaf0181d04d1dcbc02b10559feafb38
Anirudh-GHUB <anirudh.spark@gmail.com>
2022-03-24 17:03:17 +0530

    aadded editorial feature

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,13 +1,18 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import { basePath, contextPath, previewPath } from "../constants/Common";
-import Home from "../Pages/Home";
+import Home from "./../Components/Home/Home";
 
 function HomeRoutes() {
   return (
     <Router>
       <Routes>
-        <Route path="" element={<h1>Root</h1>} />
+        <Route path="" element={<Navigate replace to={basePath} />} />
         <Route path={basePath} element={<Home />} />
         <Route path={contextPath} element={<Home />} />
         <Route path={previewPath} element={<Home />} />
